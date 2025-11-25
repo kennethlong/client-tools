075b3f553ceba3f9e36f0186e4eceedf13375025
Koogie <Keegan@keplargames.studio>
2025-11-26 10:42:02 +1300

    Fix scoping on member function which doesn't compile in newer VS

--- src/engine/client/library/clientUserInterface/src/shared/core/CuiVoiceChatManager.h
+++ src/engine/client/library/clientUserInterface/src/shared/core/CuiVoiceChatManager.h
@@ -351,7 +351,7 @@ private:
 	void processVoiceChatStatus(MessageDispatch::Emitter const &, MessageDispatch::MessageBase const & message);
 	void processVCBroadcastMessage(MessageDispatch::Emitter const &, MessageDispatch::MessageBase const & message);
 
-	static void CuiVoiceChatManager::handleConsentWindow(int id, bool response);
+	static void handleConsentWindow(int id, bool response);
 
 	void onConnectionServerConnectionChanged(bool const &);
 	void onIgnoreListChanged(bool const & status);
